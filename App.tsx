da284d93d6d015f6c83897add4fcb621f19e37ea
Phil Bulleyment <philbulleyment@yahoo.co.uk>
2021-11-02 13:04:40 +0000

    moves stest search button to search

--- App.tsx
+++ App.tsx
@@ -1,32 +1,28 @@
 import React from "react";
 import { NavigationContainer, StackActions } from "@react-navigation/native";
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
 import MainStackNavigator from "./components/MainStackNavigator";
 
-
 const Drawer = createDrawerNavigator();
 
-
-
 export default function App() {
-	return (
+  return (
     <SafeAreaProvider>
-		<NavigationContainer>
-		<MainStackNavigator />
-		</NavigationContainer>
+      <NavigationContainer>
+        <MainStackNavigator />
+      </NavigationContainer>
     </SafeAreaProvider>
-
-	);
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
