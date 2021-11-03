aece86ddee6d2855bf94134ae9259304529a08f1
Phil Bulleyment <philbulleyment@yahoo.co.uk>
2021-11-03 13:05:54 +0000

    adds login facility, shows user

--- App.tsx
+++ App.tsx
@@ -1,14 +1,30 @@
 import React from "react";
+import { useState } from "react";
+import { Auth } from 'aws-amplify'
 import { NavigationContainer, StackActions } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import Amplify from 'aws-amplify'
+import config from './src/aws-exports'
 
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
 import MainStackNavigator from "./components/MainStackNavigator";
+// @ts-ignore
+import { withAuthenticator } from 'aws-amplify-react-native';
 
 const Drawer = createDrawerNavigator();
+Amplify.configure({
+  ...config,
+  Analytics: {
+    disabled: true,
+  },
+});
 
-export default function App() {
+
+
+function App() {
+  const [userName, setUserName] = useState('')
+
   return (
     <SafeAreaProvider>
       <NavigationContainer>
@@ -26,3 +42,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+export default withAuthenticator(App);
--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
+import { useState } from 'react';
+import { Auth } from 'aws-amplify'
 import { Text, View, Button, TouchableOpacity, StyleSheet } from "react-native";
 
 
 
 function HomeScreen(props: any) {
+  const [userName, setUserName] = useState('')
+
   const { navigation } = props
+
+  const getUserName =  async () => {
+    const user = await Auth.currentAuthenticatedUser();  
+    setUserName (user.username)
+  }
+  getUserName()
+  
+
+  
+
+  
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Home!</Text>
         <Text> User home page with plants</Text>
+        <Text> Hello {userName} </Text>
         
         
       </View>
