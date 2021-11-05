aefdaf43c5992ebf183000b041ace3d410673438
Phil Bulleyment <philbulleyment@yahoo.co.uk>
2021-11-05 12:41:47 +0000

    changes plant search to flat list

--- components/SingleCategoryPlantScreen.tsx
+++ components/SingleCategoryPlantScreen.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { View, Text, ScrollView, StyleSheet } from "react-native";
+import { View, Text, ScrollView, StyleSheet, List, SafeAreaView,TouchableOpacity, StatusBar } from "react-native";
 import { useState, useEffect } from "react";
 import { getPlants } from "./utils/Api";
 import { ListItem, Avatar } from "react-native-elements";
+import { FlatList } from "react-native-gesture-handler";
 
 const SingleCategoryPlantScreen = (props: any) => {
   const [plants, setPlants] = useState([]);
   const {plantCategoryId} = props.route.params
+  const [selectedId, setSelectedId] = useState(null);
+
+
 
   useEffect(() => {
     getPlants(plantCategoryId)
@@ -18,21 +22,39 @@ const SingleCategoryPlantScreen = (props: any) => {
       });
   },[]);
 
+  const Item = ({ item, onPress, backgroundColor, textColor }) => (
+    <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
+      <Text style={[styles.title, textColor]}>{item.commonName}</Text>
+      <Text style={[styles.subtitle, textColor]}>{item.botanicalName}</Text>
+      <Avatar source={{uri: item.image_url}} />
+
+    </TouchableOpacity>
+  );
+
+  const renderItem = ({ item }) => {
+    const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#d6d6d6";
+    const color = item.id === selectedId ? 'white' : 'black';
+
+    return (
+      <Item
+        item={item}
+        onPress={() => setSelectedId(item.botanicalName)}
+        backgroundColor={{ backgroundColor }}
+        textColor={{ color }}
+      />
+    );
+  };
+  
   return (
     
-      <View style={styles.subtitleView}>
-      <ScrollView style={styles.scrollView}>
-        {plants.map((plant, i) => (
-          <ListItem key={i} bottomDivider>
-            <Avatar source={{ uri: plant.image_url }} />
-            <ListItem.Content>
-              <ListItem.Title>{plant.botanicalName}</ListItem.Title>
-              <ListItem.Subtitle>{plant.commonName}</ListItem.Subtitle>
-            </ListItem.Content>
-          </ListItem>
-        ))}
-        </ScrollView>
-      </View>
+      <SafeAreaView style={styles.container}>
+      <FlatList contentContainerStyle={styles.subtitleView}
+        data={plants}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.botanicalName}
+        extraData={selectedId}
+      />
+    </SafeAreaView>
       
     
   );
@@ -45,9 +67,28 @@ const styles = StyleSheet.create({
   ratingImage: { height: 19.21, width: 100 },
   ratingText: { paddingLeft: 10, color: "grey" },
   scrollView: {
-    backgroundColor: 'pink',
+    backgroundColor: 'white',
     marginHorizontal: 20,
+  },
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderRadius:10,
+  },
+  title: {
+    fontSize: 15,
+    fontWeight:'bold',
+  },
+  subtitle: {
+    fontSize: 10,
+    
   },
 });
 
 export default SingleCategoryPlantScreen;
+
