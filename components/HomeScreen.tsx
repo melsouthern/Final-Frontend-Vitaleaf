41fea630fdbc99003542f81860a44bb4b8d174b3
Melissa Southern <melissa_southern@hotmail.co.uk>
2021-11-09 16:56:04 +0000

    testing logo homepage

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -22,28 +22,15 @@ import { useIsFocused } from "@react-navigation/native";
 
 function HomeScreen(props: any) {
   const { userName, setUserName } = useContext(UserContext);
-  
-
-  
-
-  
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.titletext}>vitaleaf</Text>
-
-      
+      <Image source={require("../assets/IMG_0308_5.png")} style={styles.logo} />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  userPlantView: {
-    paddingLeft: 10,
-    paddingTop: 5,
-  },
-  ratingImage: { height: 19.21, width: 100 },
-  ratingText: { paddingLeft: 10, color: "grey" },
   scrollView: {
     backgroundColor: "#082d0fff",
     marginHorizontal: 20,
@@ -51,13 +38,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
-  },
-  item: {
-    flex: 1 / 2,
-    padding: 10,
-    marginVertical: 8,
-    marginHorizontal: 20,
-    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#004346",
   },
   title: {
     fontSize: 13,
@@ -69,7 +52,14 @@ const styles = StyleSheet.create({
   titletext: {
     fontSize: 25,
   },
-  logo: {},
+  logo: {
+    flex: 1,
+    resizeMode: "contain",
+    flexDirection: "column",
+    // aspectRatio: 0.6,
+    height: "100%",
+    width: "80%",
+  },
 });
 
 export default HomeScreen;
