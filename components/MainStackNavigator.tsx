4c3abf04ae44c989218f93333827ea44c8ad9ddd
John Ingham <johnaingham@yahoo.co.uk>
2021-11-10 14:10:30 +0000

    resolving merge conflict

--- components/MainStackNavigator.tsx
+++ components/MainStackNavigator.tsx
@@ -1,57 +1,75 @@
-import React from 'react';
-import { useState } from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-import HomeScreen from './HomeScreen';
-import CalendarScreen from './CalendarScreen';
-import MainTabNavigator from './MainTabNavigator';
-import SearchScreen from './SearchScreen';
-import CameraScreen from './CameraScreen';
-import LightMeterScreen from './LightMeterScreen';
-import SingleLookedUpPlantScreen from './SingleLookedUpPlantScreen';
-import SingleCategoryPlantScreen from './SingleCategoryPlantScreen';
-import SearchedForPlants from './SearchedForPlants'
-import {
-      getFocusedRouteNameFromRoute,
-  } from '@react-navigation/native';
+import React from "react";
+import { useState } from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from "./HomeScreen";
+import CalendarScreen from "./InventoryScreen";
+import MainTabNavigator from "./MainTabNavigator";
+import SearchScreen from "./SearchScreen";
+import CameraScreen from "./CameraScreen";
+import LightMeterScreen from "./LightMeterScreen";
+import SingleLookedUpPlantScreen from "./SingleLookedUpPlantScreen";
+import SingleCategoryPlantScreen from "./SingleCategoryPlantScreen";
+import SearchedForPlants from "./SearchedForPlants";
+import SingleUserPlant from "./SingleUserPlant";
+import { Image, StyleSheet, Text, View } from "react-native";
+import vitaleafNarrow from "../assets/vitaleafNarrow3.png";
 
-const Stack = createStackNavigator()
+import { getFocusedRouteNameFromRoute } from "@react-navigation/native";
 
-function getHeaderTitle(route:object) {
-    // In case the focused route is not found, assume it's the first screen
-    return getFocusedRouteNameFromRoute(route) ?? 'Home';
-  }
+const Stack = createStackNavigator();
+
+function getHeaderTitle(route: object) {
+  // In case the focused route is not found, assume it's the first screen
+  return getFocusedRouteNameFromRoute(route) ?? "Home";
+}
 
 function MainStackNavigator() {
   
 
-  //const [plantCategory, setPlantCategory] = useState<string | null>("")
-  
   return (
-    
-      <Stack.Navigator screenOptions={{
+    <Stack.Navigator
+      screenOptions={{
         gestureEnabled: true,
         gestureDirection: "horizontal",
+
         headerStyle: {
-          backgroundColor: '#17B890'
+          backgroundColor: "#17B890",
         },
+        headerBackground: () => (
+          <Image
+            style={{ flex: 1, height: 25, width: 380 }}
+            source={vitaleafNarrow}
+          />
+        ),
+
         headerTitleStyle: {
-          fontWeight: 'bold'
+          fontFamily: "Futura",
         },
-        headerTintColor: '#ffffff'
+        headerTintColor: "#ffffff",
       }}
-      
-      >
-        <Stack.Screen name='Main' component={MainTabNavigator} options={({route}) => ({
-          title: getHeaderTitle(route),
-        })}  />
-        
-        <Stack.Screen name="Single Looked Up Plant" component={SingleLookedUpPlantScreen} />
-        <Stack.Screen name="Single Plant Category" component={SingleCategoryPlantScreen} />
-        <Stack.Screen name="Searched For Plants" component={SearchedForPlants} />
-      </Stack.Navigator>
-    
-  )
+    >
+      <Stack.Screen
+        name="Main"
+        component={MainTabNavigator}
+        options={({ route }) => ({
+          //title: getHeaderTitle(route),
+          title: null,
+        })}
+      />
+
+      <Stack.Screen
+        name="Single Looked Up Plant"
+        component={SingleLookedUpPlantScreen}
+      />
+      <Stack.Screen
+        name="Single Plant Category"
+        component={SingleCategoryPlantScreen}
+      />
+      <Stack.Screen name="Searched For Plants" component={SearchedForPlants} />
+      <Stack.Screen name="Single User Plant" component={SingleUserPlant} />
+    </Stack.Navigator>
+  );
 }
 
-export default MainStackNavigator
+export default MainStackNavigator;
