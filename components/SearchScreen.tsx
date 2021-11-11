fd417cdc6d7ecb215363824d0af26c4b4d192c2f
Melissa Southern <melissa_southern@hotmail.co.uk>
2021-11-11 12:28:16 +0000

    tweaked carousel

--- components/SearchScreen.tsx
+++ components/SearchScreen.tsx
@@ -29,13 +29,16 @@ const SearchScreen = (props: any) => {
     setIndexSelected(indexSelected);
   };
   const { width } = Dimensions.get("window");
-  // const SPACING = 10;
-  // const THUMB_SIZE = 80;
+  const window = width * 0.88;
+  const sliderWindow = width * 1;
+
+  const SPACING = 10;
+  const THUMB_SIZE = 80;
 
   const IMAGES = {
-    image1: require("../assets/Indoor-Flowers.jpg"),
-    image2: require("../assets/foliage-house-plants-1.jpg"),
-    image3: require("../assets/cacti_2.jpg"),
+    image1: require("../assets/flowering-house-plants-7.jpeg"),
+    image2: require("../assets/foilage-house-plants-2.jpeg"),
+    image3: require("../assets/cacti-and-succs-house-plants.jpeg"),
   };
 
   const [images, setImages] = useState([
@@ -57,12 +60,13 @@ const SearchScreen = (props: any) => {
         value={searchQuery}
         onSubmitEditing={(searchQuery) => handleSearch(searchQuery)}
       />
-      <View>
+      <View style={styles.carouselContainer}>
         <Carousel
+          style={styles.carousel}
           layout="default"
           data={images}
-          sliderWidth={width}
-          itemWidth={width}
+          sliderWidth={window}
+          itemWidth={window}
           renderItem={({ item, index }) => (
             <TouchableOpacity
               style={styles.touchable}
@@ -93,8 +97,26 @@ const SearchScreen = (props: any) => {
 };
 
 const styles = StyleSheet.create({
+  carouselContainer: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+  },
+  //carouselContainer has buttons - don't delete
+
+  carousel: {
+    // flex: 1,
+    // flexDirection: "row",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
   container: {
     flex: 1,
+    flexDirection: "column",
+    // justifyContent: "space-between",
+    // alignContent: "center",
     alignItems: "center",
     backgroundColor: "#EFF5E7",
   },
@@ -102,7 +124,9 @@ const styles = StyleSheet.create({
     paddingTop: 15,
     fontSize: 20,
     color: "#EFF5E7",
-    paddingBottom: 5,
+    // paddingBottom: 5,
+    // minHeight: 50,
+    // height: 60,
     //#09BC8A
   },
   searchbar: {
@@ -110,13 +134,14 @@ const styles = StyleSheet.create({
     marginTop: 40,
   },
   touchable: {
-    width: 180,
-    height: 250,
-    aspectRatio: 1 * 1.3,
+    width: "100%",
+    // height: 250,
+    aspectRatio: 1 * 1.5,
     overflow: "hidden",
     marginTop: "20%",
     alignItems: "center",
-    marginHorizontal: 20,
+    alignContent: "center",
+    // marginHorizontal: "5%",
     borderRadius: 10,
     backgroundColor: "#004346",
     shadowColor: "#000",
@@ -126,11 +151,15 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.48,
     shadowRadius: 11.95,
+    elevation: 18,
   },
   categoryImage: {
+    // flex: 1,
     width: "100%",
-    height: "100%",
+    maxHeight: "100%",
     resizeMode: "stretch",
+    // height: 600,
+    // alignContent: "center",
   },
 });
 
