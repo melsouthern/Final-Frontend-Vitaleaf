9c57f3497e80a102dd5a79483125c37a5f6bfcf4
Hothyfa Elbeera <h.s.elbeera@gmail.com>
2021-11-11 11:48:34 +0000

    user page worked on

--- components/ProfileScreen.tsx
+++ components/ProfileScreen.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useContext } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import SingleLookedUpPlantScreen from "./SingleLookedUpPlantScreen";
 import { UserContext, UserProvider } from "./utils/User";
@@ -12,9 +12,15 @@ const Stack = createStackNavigator();
 function ProfileScreen(props: any) {
   const { userName, setUserName } = useContext(UserContext);
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Profile Page</Text>
-      <Text>{userName}'s Profile Here</Text>
+    <View style={styles.container}>
+      <View>
+      <Image
+          source={require('../assets/user.png')}
+          style={{borderRadius: 150}}
+        />
+      </View>
+      <Text style={styles.text}>Username: {userName}</Text>
+      
       <TouchableOpacity
         style={styles.buttonContainer}
         onPress={() => signOut()}
@@ -29,22 +35,36 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#ebebeb",
+    backgroundColor: "#EFF5E7",
   },
   text: {
-    color: "#101010",
-    fontSize: 24,
+    marginTop: 20,
+    color: "#004346",
+    fontSize: 14,
     fontWeight: "bold",
   },
   buttonContainer: {
-    backgroundColor: "#222",
+    backgroundColor: "#09BC8A",
+    alignSelf: "center",
+    alignItems: "center",
+    width: "35%",
     borderRadius: 5,
     padding: 10,
     margin: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+	  width: 0,
+  	height: 9,
+    },
+    shadowOpacity: 0.48,
+    shadowRadius: 11.95,  
+    elevation: 18,
+    marginTop: 150,
   },
   buttonText: {
     fontSize: 20,
-    color: "#fff",
+    fontWeight: "500",
+    color: "#004346",
   },
 });
 
